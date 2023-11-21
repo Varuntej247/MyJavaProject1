63c060fa538322cb468185126186028337a0bb6b
Varuntej247 <varuntejkandikonda.00@gmail.com>
2023-11-21 11:19:57 +0530

    Welcome.java is updated

--- src/com/edubridge/app1/Welcome.java
+++ src/com/edubridge/app1/Welcome.java
@@ -3,6 +3,7 @@ public class Welcome {
 
 	public static void main(String[] args) {
 		System.out.println("welcome to java");
+		System.out.println("welcome to python");
 	}
 
 }
--- src/module-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module MyJavaProject1 {
-}
